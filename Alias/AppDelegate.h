0687a1044f73d0302d3938d6a6c3452b3ad011c4
Alex Motarykin <orwester@gmail.com>
2015-08-05 13:07:42 +0300

    Initial Commit

---
+++ Alias/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alias
+//
+//  Created by Alex Motor on 05.08.15.
+//  Copyright (c) 2015 Alex Motor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alias/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alias
+//
+//  Created by Alex Motor on 05.08.15.
+//  Copyright (c) 2015 Alex Motor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
