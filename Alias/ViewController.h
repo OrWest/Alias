e108963fd7726c40d60c1224e7995a79fc319954
Alex Motarykin <orwester@gmail.com>
2015-08-05 17:28:02 +0300

    Add Segue

--- Alias/ViewController.h
+++ Alias/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction) pressOKAction:(UIButton*) sender;
 
 @end
 
--- Alias/ViewController.m
+++ Alias/ViewController.m
@@ -25,4 +25,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Actions
+
+- (IBAction) pressOKAction:(UIButton *)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
 @end
